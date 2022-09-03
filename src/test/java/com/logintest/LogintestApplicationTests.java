404ddc8bb80a063c8b49bae0793fdb8cedc06cc0
schoi567 <89711765+schoi567@users.noreply.github.com>
2022-09-03 10:16:42 -0400

    first commit

---
+++ src/test/java/com/logintest/LogintestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogintestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
